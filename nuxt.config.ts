4dfdbace3c515b5cceef7f2dc46d6c5c1479d0e3
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-05-23 14:10:15 +0530

    changes the first 40 lines
    changes the first 40 lines

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,28 @@
+import { defineNuxtConfig } from '@nuxtjs/composition-api';
+
 export default defineNuxtConfig({
   app: {
     head: {
-      titleTemplate: "%s - amvstrm",
-      title: "amvstrm",
+      titleTemplate: "%s - AnimeVerse",
+      title: "AnimeVerse",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
           name: "description",
-          content: "amvstrm - A streaming service for weebo...",
+          content: "AnimeVerse - A streaming service for weebo...",
         },
         {
           name: "keywords",
           content:
-            "amvstrm, amvstr, amvstreaming, anime streaming site, anime, free anime",
+            "AnimeVerse, anime streaming site, anime, free anime",
         },
         { property: "og:type", content: "website" },
         { property: "og:url", content: "/" },
-        { property: "og:title", content: "Home - amvstrm" },
+        { property: "og:title", content: "Home - AnimeVerse" },
         {
           property: "og:description",
-          content: "amvstrm - A streaming service for weebo...",
+          content: "AnimeVerse - A streaming service for weebo...",
         },
         {
           property: "og:image",
@@ -28,10 +30,10 @@ export default defineNuxtConfig({
         },
         { property: "twitter:card", content: "summary_large_image" },
         { property: "twitter:url", content: "/" },
-        { property: "twitter:title", content: "Home - amvstrm" },
+        { property: "twitter:title", content: "Home - AnimeVerse" },
         {
           property: "twitter:description",
-          content: "amvstrm - A streaming service for weebo...",
+          content: "AnimeVerse - A streaming service for weebo...",
         },
         {
           property: "twitter:image",
